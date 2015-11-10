0aff619fd03030c7cd85c27790afe96d2e1e4ba2
hostrue <watson9281@gmail.com>
2015-11-11 01:15:51 +0800

    sort virus file

--- findSearchNum.py
+++ findSearchNum.py
@@ -7,13 +7,17 @@ Created on Sun Nov 08 15:06:08 2015
 
 import urllib
 import re
+import scipy.stats as ss
 
 virusDic = {}
 
 fvirus = open('vnVSvnc.txt')
 for i in fvirus.readlines():
     fName = re.findall('\w*',i)[0]
-    vName = re.findall('\w*',i)[7].split('_')[0]
+    vName1 = re.findall('\w*',i)[7].split('_')[0]
+    vName2 = re.findall('\w*',i)[7].split('_')[1]
+    vName = '' + vName1 + ' ' + vName2
+    vName = vName1
     if virusDic.has_key(vName):
         virusDic[vName].append(fName)
     else:
@@ -36,4 +40,17 @@ for query in virusDic.keys():
         num = 0
     else:
         num = re.findall('\d*', numRaw[0])[3]
-    numList.append(num)
+    numList.append(int(num))
+    print query, num
+    
+ind = list(ss.rankdata(numList,method='ordinal'))
+
+queryOrder = open('virusqueryOrder.txt', 'w')
+queryOrder1 = open('virusqueryOrder1.txt', 'w')
+for i in reversed(range(len(queryList))):
+    id = int(ind.index(i+1))
+    for fileName in virusDic[queryList[id]]:
+        queryOrder.write('{0}.txt\t{1}\t{2}\n'.format(fileName, queryList[id], numList[id]))
+        queryOrder1.write('{0}.txt\n'.format(fileName))
+queryOrder.close()
+queryOrder1.close()
