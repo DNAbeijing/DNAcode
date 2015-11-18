c24d05ee82a442d1e368f4483a0f2f6cc1aa7f6e
hostrue <watson9281@gmail.com>
2015-11-18 11:08:13 +0800

    a

--- findVirus.m
+++ findVirus.m
@@ -1,11 +1,19 @@
 % 取距离最小的前k个作为候选的感染源
-k = 1;
+k = 100;
+nRow = 67;
+nCol = 1415;
+mat = zeros(nRow,nCol);
 
-[nRow, nCol] = size(Coutput);
+for i = 1:nRow
+    for j = 1:nCol
+        mat(i,j) = Doutput((i-1)*nRow+j,3);
+    end
+end
+
 argminVec = [];
 
 for i = 1:nRow
-    [val, id] = sort(Coutput(i,:));
+    [val, id] = sort(mat(i,:));
     if min(val) < 0.001
         argminVec = [argminVec; id(1:k)];
     end
@@ -17,5 +25,5 @@ hist(argminVec(:), nCol);
 
 % 做出最小距离的分布图
 figure(2)
-sortMinDist = sort(min(Coutput, [], 2));
+sortMinDist = sort(min(mat, [], 2));
 plot(sortMinDist(1:end-100));
